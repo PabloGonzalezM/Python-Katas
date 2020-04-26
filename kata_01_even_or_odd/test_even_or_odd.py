69d79fd25a96a34f04120c58f7d2b4915b803aa3
PabloGonzalezM <pablogonzalezmolina@hotmail.es>
2020-04-26 21:01:04 +0200

    Refactor(Kata 01): Change random choice for randrange

--- kata_01_even_or_odd/test_even_or_odd.py
+++ kata_01_even_or_odd/test_even_or_odd.py
@@ -1,5 +1,5 @@
 import unittest
-from random import choice
+from random import randrange
 
 from kata_01_even_or_odd.even_or_odd import even_or_odd, lambda_even_or_odd
 
@@ -7,12 +7,12 @@ from kata_01_even_or_odd.even_or_odd import even_or_odd, lambda_even_or_odd
 class TestEvenOrOdd(unittest.TestCase):
 
 	def test_is_even(self):
-		self.assertEqual(even_or_odd(choice(range(-100, 100, 2))), 'Even')
-		self.assertEqual(lambda_even_or_odd(choice(range(-100, 100, 2))), 'Even')
+		self.assertEqual(even_or_odd(randrange(-100, 100, 2)), 'Even')
+		self.assertEqual(lambda_even_or_odd(randrange(-100, 100, 2)), 'Even')
 
 	def test_is_odd(self):
-		self.assertEqual(even_or_odd(choice(range(-99, 99, 2))), 'Odd')
-		self.assertEqual(lambda_even_or_odd(choice(range(-99, 99, 2))), 'Odd')
+		self.assertEqual(even_or_odd(randrange(-99, 99, 2)), 'Odd')
+		self.assertEqual(lambda_even_or_odd(randrange(-99, 99, 2)), 'Odd')
 
 	def test_wrong_type(self):
 		data = "bad_input"
